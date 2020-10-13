38d5ad67971b5af9b35bc7c4d673d3ad2a8e4efd
Anya <ania.leonova@gmail.com>
2020-10-13 13:00:36 -0700

    Can trigger an event when player enters NPC's radius

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name + " has entered the collision.");
+    }
+
+}
