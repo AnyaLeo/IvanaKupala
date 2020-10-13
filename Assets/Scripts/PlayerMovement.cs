ed135045714ed24559962ac2abe6d58ff9782f93
Anya <ania.leonova@gmail.com>
2020-10-13 12:39:25 -0700

    Added a player that can be moved with WASD keys

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // The motor that drives our player
+    public Rigidbody controller;
+
+    public float speed = 6f;
+
+    private Vector3 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+
+        // X axis (horizontal) and Z axis (vertical) input
+        // Y axis for moving up and down
+        movement.x = -Input.GetAxisRaw("Horizontal");
+        movement.z = -Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement
+        controller.MovePosition(
+                 controller.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
